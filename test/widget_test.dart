90ac0d2386e6ddecc9819ea60c3c65c38afd1e9c
Charbel Maarbes <charbelmaarbess@gmail.com>
2026-06-10 04:04:01 +0300

    mock shared preferences in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:srood_live/main.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() async {
+    SharedPreferences.setMockInitialValues({});
+
+    await Supabase.initialize(
+      url: 'https://test.supabase.co',
+      anonKey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.test.test',
+    );
+  });
+
   testWidgets('SrOOd Live app loads', (WidgetTester tester) async {
     await tester.pumpWidget(const SrOOdLiveApp());
     await tester.pump();
 
     expect(find.byType(MaterialApp), findsOneWidget);
-    expect(find.text('Voice rooms. Gifts. Prestige.'), findsOneWidget);
 
     await tester.pump(const Duration(seconds: 3));
     await tester.pump();
